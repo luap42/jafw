6ca5e06fdc741a14d2fb9dd2eba91715d39f8d8a
luap42 <pstrobach15+luap42@gmail.com>
2025-04-06 02:57:37 +0200

    Implement variables

--- jafw_template.py
+++ jafw_template.py
@@ -1,4 +1,7 @@
 import os
+import re
+
+VARIABLE_SUBSTITUTION = re.compile(r'\$([A-za-z_]+)|\$\{(.+?)\}')
 
 def render_template(body, meta):
     template_file = get_template_file(meta.get('template', 'default'))
@@ -13,4 +16,23 @@ def get_template_file(fn):
 
 
 def apply_data_to_templa_file(template_file, body, meta):
+    for var in re.finditer(VARIABLE_SUBSTITUTION, template_file):
+        orig = var[0]
+        varname = var[1] if var[1] is not None else var[2]
+        template_file = template_file.replace(orig, load_var(varname, body, meta))
+
     return template_file
+
+
+def load_var(varname, body, meta):
+    if varname == 'Body':
+        return body
+    
+    elif varname == 'Generator':
+        return 'jafw'
+
+    else:
+        if varname in meta.keys():
+            return meta[varname]
+        
+    return f'[not found: {varname}]'
